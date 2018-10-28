e1ba16f1d8d277fcfb19c8f274eac510228e4c71
jiangkaiwen <894912881@qq.com>
2018-10-28 18:07:39 +0800

    init

---
+++ monitor-tableview-navigationDemo/monitor-tableview-navigationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  monitor-tableview-navigationDemo
+//
+//  Created by mac on 2018/10/28.
+//  Copyright © 2018 姜凯文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ monitor-tableview-navigationDemo/monitor-tableview-navigationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  monitor-tableview-navigationDemo
+//
+//  Created by mac on 2018/10/28.
+//  Copyright © 2018 姜凯文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
